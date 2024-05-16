5ad6c5d7781ebf884fd84c9c4fe182076567bd6e
remi.germe <remi.germe@gmail.com>
2024-05-16 17:24:54 +0200

    new marteleur struct

--- src/objects/marteleur.cpp
+++ src/objects/marteleur.cpp
@@ -36,3 +36,66 @@ mesh mesh_primitive_conetronqued(float radius_bas, float z_bas,
     m.fill_empty_field();
     return m;
 };
+
+Marteleur::Marteleur(bool _unused) {
+    // Create the hierarchy
+    // ************************************ //
+
+    // Initialize the temporary mesh_drawable that will be inserted in the
+    // hierarchy
+    mesh_drawable tube_central;
+    mesh_drawable corrole;
+    mesh_drawable disc1;
+    mesh_drawable disc2;
+    mesh_drawable bas;
+    mesh_drawable haut;
+    mesh_drawable tete;
+    mesh_drawable cone_bas;
+    mesh_drawable pas_de_vis;
+    mesh_drawable disc3;
+    mesh_drawable pas_bas;
+
+    // Create the geometry of the meshes
+
+    tube_central.initialize_data_on_gpu(
+        mesh_primitive_cylinder(0.05f, {0, 0, 0}, {0, 0, 1.5}));
+    corrole.initialize_data_on_gpu(
+        mesh_primitive_cylinder(0.3f, {0, 0, 0}, {0, 0, 0.05}));
+    disc1.initialize_data_on_gpu(mesh_primitive_disc(0.3f));
+    disc2.initialize_data_on_gpu(mesh_primitive_disc(0.3f));
+    bas.initialize_data_on_gpu(
+        mesh_primitive_conetronqued(0.21, 0, 0.25, 0.25));
+    haut.initialize_data_on_gpu(
+        mesh_primitive_conetronqued(0.25, 0, 0.21, 0.55));
+    tete.initialize_data_on_gpu(
+        mesh_primitive_ellipsoid({0.21, 0.21, 0.21}, {0, 0, 0}));
+    cone_bas.initialize_data_on_gpu(
+        mesh_primitive_conetronqued(0.15, 0, 0.21, 0.1));
+    pas_de_vis.initialize_data_on_gpu(
+        mesh_primitive_cylinder(0.15, {0, 0, 0}, {0, 0, 0.15}));
+    disc3.initialize_data_on_gpu(mesh_primitive_disc(0.25f));
+    pas_bas.initialize_data_on_gpu(
+        mesh_primitive_conetronqued(0.05, 0, 0.15, 0.05));
+
+    // Set the color of some elements
+    haut.material.color = {1.0f, 0.5f, 0.1f};
+
+    // Add the elements in the hierarchy
+
+    marteleur.add(tube_central, "tube_central");
+    marteleur.add(corrole, "corrole", "tube_central", {0, 0, 1});
+    marteleur.add(disc1, "disc1", "corrole", {0, 0, 0.05});
+    marteleur.add(disc2, "disc2", "corrole", {0, 0, 0});
+    marteleur.add(bas, "bas", "tube_central", {0, 0, 0.75});
+    marteleur.add(haut, "haut", "tube_central", {0, 0, 1});
+    marteleur.add(tete, "tete", "haut", {0, 0, 0.55});
+    marteleur.add(disc3, "disc3", "haut", {0, 0, 0});
+    marteleur.add(cone_bas, "cone_bas", "tube_central", {0, 0, 0.65});
+    marteleur.add(pas_de_vis, "pas_de_vis", "tube_central", {0, 0, 0.50});
+    marteleur.add(pas_bas, "pas_bas", "tube_central", {0, 0, 0.45});
+}
+
+Marteleur::Marteleur() {
+    // We're not supposed to use this one !
+    assert(false);
+}
--- src/objects/marteleur.hpp
+++ src/objects/marteleur.hpp
@@ -1,6 +1,21 @@
+#pragma once
+
 #include "cgp/cgp.hpp"
 
-using cgp::mesh;
+using cgp::hierarchy_mesh_drawable;
+using cgp::vec3;
 
-mesh mesh_primitive_conetronqued(float radius_bas, float z_bas,
-                                 float radius_haut, float z_haut);
+struct Marteleur {
+    hierarchy_mesh_drawable marteleur;
+
+    // Initialize the mesh of the marteleur and the mesh_drawable
+    // Useless argument but used to differ this ctor from the next one
+    // See below the reason
+    Marteleur(bool _unused);
+
+    // Just to avoid compilation warnings
+    // Do nothing and should do nothing
+    // else is called before some initialization stuff
+    // in cgp and segfault yeepee
+    Marteleur();
+};
