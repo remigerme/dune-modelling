0847fe29baebc9ba5931239b47781b4212cc1a4e
remi.germe <remi.germe@gmail.com>
2024-05-16 16:52:42 +0200

    cliff using struct

--- src/terrain/cliff.hpp
+++ src/terrain/cliff.hpp
@@ -2,4 +2,24 @@
 
 #include "cgp/cgp.hpp"
 
-cgp::mesh create_cliff_mesh(float cliff_uv_range);
+using cgp::mesh;
+using cgp::mesh_drawable;
+using cgp::vec3;
+
+struct Cliff {
+    vec3 cliff_scale;
+    mesh cliff_mesh;
+    mesh_drawable cliff_drawable;
+
+    // Initialize the mesh of the cliff and the mesh_drawable
+    // We scale the mesh_drawable by cliff_scale
+    // We change (u, v) from [0,1] to [0, uv_range]
+    // To allow the texture to repeat on a single grid
+    Cliff(vec3 cliff_scale, float uv_range);
+
+    // Just to avoid compilation warnings
+    // Do nothing and should do nothing
+    // else is called before some initialization stuff
+    // in cgp and segfault yeepee
+    Cliff();
+};
