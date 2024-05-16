c0e994809494be2d707b971ec632882c09410f0d
isa-j <lilicastor04@gmail.com>
2024-05-16 17:08:47 +0200

    New beautyyyyy worm

--- src/objects/worm.cpp
+++ src/objects/worm.cpp
@@ -1,26 +1,133 @@
 #include "worm.hpp"
+#include "../noise/perlin.hpp"
 
 using namespace cgp;
 
-cgp::mesh create_worm(float radius, float length) {
-    // Shortcuts variables
-    float x_min = -length / 2;
-    float x_max = length / 2;
+// Fonction f(x) = r*exp(-ax^2), pour faire la forme du ver
+//         _____
+//        /     \
+//       /
+//______/
+//
+float worm_shape(float a, float r, float x) {
+    float f_exp = r * exp(-a * x * x);
+    float lim = 0.75 * r;
+    if (lim < f_exp) {
+        return (f_exp);
+    } else {
+        return (lim);
+    }
+}
 
-    // The pointy tail
-    mesh tail =
-        mesh_primitive_cone(radius, length / 4, {x_min, 0, 0}, {-1, 0, 0});
+float mouth_shape(float x_mouth, float x_bottom, float R, float r) {
+    float mouth_depth = x_mouth - x_bottom;
+    float a = mouth_depth / (R * R);
+    return (a * r * r + x_bottom);
+}
 
-    // The body
-    vec3 p1 = {x_min, 0, 0};
-    vec3 p2 = {x_max, 0, 0};
-    mesh body = mesh_primitive_cylinder(radius, p1, p2);
+cgp::mesh create_worm_body(float radius, float h, int length_h, float x_mouth,
+                           float a) {
+    // h le pas, et lenght_h le nombre de pas qui est donc proportionnel à la
+    // longeur
 
-    // The mouth and the teeth
-    mesh mouth = mesh_primitive_disc(radius, {x_max, 0, 0}, {1, 0, 0});
+    mesh m;
+    int polygone = 30;
 
-    mesh worm = body;
-    worm.push_back(tail);
-    worm.push_back(mouth);
-    return worm;
+    for (int k = 0; k < length_h; k++) {
+        float abscicsse = x_mouth - k * h * length_h;
+        float shape = worm_shape(a, radius, abscicsse);
+        for (int i = 0; i < polygone; i++) {
+            float frac = 2 * Pi / polygone;
+            float y = shape * cos(i * frac);
+            float z = shape * sin(i * frac);
+
+            m.position.push_back(vec3{abscicsse, y, z});
+        }
+    }
+
+    perlin_noise_parameters p = {0.3f, 2.0f, 3, 1};
+    for (int kh = 0; kh < length_h; kh++) {
+        for (int ku = 0; ku < polygone; ku++) {
+            int idx = kh * polygone + ku;
+            float z = m.position[idx].z;
+            float y = m.position[idx].y;
+            float r = sqrt(y * y + z * z);
+            float p_noise = noise_perlin(m.position[idx], p.octave,
+                                         p.persistency, p.frequency);
+
+            if (kh < 10) {
+                float coeff_perlin = 0.2 + 0.1 * kh / 10;
+                m.position[idx].y = (1 + coeff_perlin * p_noise) * y;
+                m.position[idx].z = (1 + coeff_perlin * p_noise) * z;
+            } else {
+                float coeff_perlin = 0.3;
+                m.position[idx].y = (1 + coeff_perlin * p_noise) * y;
+                m.position[idx].z = (1 + coeff_perlin * p_noise) * z;
+            }
+        }
+    }
+
+    for (int kh = 0; kh < length_h - 1; kh++) {
+        for (int ku = 0; ku < polygone - 1; ku++) {
+
+            uint3 triangle_1 = {polygone * kh + ku + 1, polygone * kh + ku,
+                                polygone * (kh + 1) + ku};
+            uint3 triangle_2 = {polygone * kh + ku + 1,
+                                polygone * (kh + 1) + ku,
+                                polygone * (kh + 1) + ku + 1};
+            m.connectivity.push_back(triangle_1);
+            m.connectivity.push_back(triangle_2);
+        }
+        uint3 triangle_1 = {polygone * kh, polygone * (kh + 1) - 1,
+                            polygone * (kh + 1)};
+        uint3 triangle_2 = {polygone * (kh + 1), polygone * (kh + 1) - 1,
+                            polygone * (kh + 2) - 1};
+        m.connectivity.push_back(triangle_1);
+        m.connectivity.push_back(triangle_2);
+    }
+
+    m.fill_empty_field();
+    return m;
+}
+
+cgp::mesh create_worm_head(cgp::mesh body, float x_mouth, float x_bottom) {
+
+    mesh m;
+    int polygone = 30;
+    int concentique = 15;
+
+    for (int kh = 0; kh < concentique + 1; kh++) {
+        for (int ku = 0; ku < polygone; ku++) {
+            float rayon = sqrt(body.position[ku].y * body.position[ku].y +
+                               body.position[ku].z * body.position[ku].z);
+            float frac = 2 * Pi / polygone;
+            float r = rayon * (concentique - kh) / concentique;
+            float depth = mouth_shape(x_mouth, x_bottom, rayon, r);
+            float y = r * cos(ku * frac);
+            float z = r * sin(ku * frac);
+            m.position.push_back(vec3{depth, y, z});
+        }
+    }
+
+    for (int kh = 0; kh < concentique; kh++) {
+        for (int ku = 0; ku < polygone - 1; ku++) {
+
+            uint3 triangle_1 = {polygone * kh + ku + 1,
+                                polygone * (kh + 1) + ku, polygone * kh + ku};
+            uint3 triangle_2 = {polygone * kh + ku + 1,
+                                polygone * (kh + 1) + ku + 1,
+                                polygone * (kh + 1) + ku};
+            m.connectivity.push_back(triangle_1);
+            m.connectivity.push_back(triangle_2);
+        }
+        uint3 triangle_1 = {polygone * kh, polygone * (kh + 1),
+                            polygone * (kh + 1) - 1};
+        uint3 triangle_2 = {polygone * (kh + 1), polygone * (kh + 2) - 1,
+                            polygone * (kh + 1) - 1};
+        m.connectivity.push_back(triangle_1);
+        m.connectivity.push_back(triangle_2);
+    }
+
+    m.fill_empty_field();
+    return m;
 }
--- src/objects/worm.hpp
+++ src/objects/worm.hpp
@@ -1,3 +1,9 @@
 #include "cgp/cgp.hpp"
 
-cgp::mesh create_worm(float radius, float length);
+float worm_shape(float a, float r, float x);
+float mouth_shape(float x_mouth, float x_bottom, float R, float r);
+
+cgp::mesh create_worm_body(float radius, float h, int length_h, float x_mouth,
+                           float a);
+
+cgp::mesh create_worm_head(cgp::mesh body, float x_mouth, float x_mottom);
