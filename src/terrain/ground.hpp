b985620ebb62223e12acbbf772e084d443422cc5
remi.germe <remi.germe@gmail.com>
2024-05-16 16:03:01 +0200

    new ground struct header

--- src/terrain/ground.hpp
+++ src/terrain/ground.hpp
@@ -2,7 +2,16 @@
 
 #include "cgp/cgp.hpp"
 
-// Initialize the mesh of the ground
-// To allow the texture to repeat on a single grid
-// We change (u, v) from [0,1] to [0, uv_range]
-cgp::mesh create_ground_mesh(float uv_range);
+struct Ground {
+    cgp::mesh ground_mesh;
+    cgp::mesh_drawable ground_drawable;
+
+    // Initialize the mesh of the ground and the mesh_drawable
+    // We scale the mesh_drawable by ground_size
+    // We change (u, v) from [0,1] to [0, uv_range]
+    // To allow the texture to repeat on a single grid
+    Ground(float ground_size, float uv_range);
+
+    // Ctor with default values (1, 1)
+    Ground();
+};
