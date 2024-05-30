b2efc5a677335c538dc0cb75f5d9cc61d4e524cd
remi.germe <remi.germe@gmail.com>
2024-05-30 22:35:13 +0200

    improved draft

--- shaders/worm/dust.vert.glsl
+++ shaders/worm/dust.vert.glsl
@@ -8,7 +8,7 @@ layout (location = 1) in vec3 vertex_normal;   // vertex normal in local space  
 layout (location = 2) in vec3 vertex_color;    // vertex color      (r,g,b)
 layout (location = 3) in vec2 vertex_uv;       // vertex uv-texture (u,v)
 layout (location = 4) in vec3 pos; 
-layout (location = 5) in vec4 phong;
+layout (location = 5) in vec2 alpha;
 
 // Output variables sent to the fragment shader
 out struct fragment_data
@@ -25,13 +25,26 @@ uniform mat4 model; // Model affine transform matrix associated to the current s
 uniform mat4 view;  // View matrix (rigid transform) of the camera
 uniform mat4 projection; // Projection (perspective or orthogonal) matrix of the camera
 uniform float time; // Time used to animate the dust
-uniform float layer; // Index of the dust layer
+uniform float crand ; // Random float in [0, 1] generated in display_frame
+
+// Random noise - https://stackoverflow.com/questions/4200224/random-noise-functions-for-glsl
+float PHI = 1.61803398874989484820459;
+float gold_noise(in vec2 xy, in float seed) {return fract(tan(distance(xy * PHI, xy) * seed) * xy.x);}
+float rand(in float n) { return gold_noise(vec2(n,n+0.5), 1.0); }
+
+
+vec3 small_disturbance(in float t, in vec3 pos) {
+	vec3 s = vec3(sin(10 * rand(gl_InstanceID) * t), cos(t), 0);
+	return s;
+}
 
 
 void main()
 {
 	// The position of the vertex in the world space
-	vec4 position = model * vec4(vertex_position, 1.0) + vec4(pos, 1.0);
+	vec4 position = model * vec4(vertex_position, 1);
+	position += vec4(pos, 1);
+	position += vec4(small_disturbance(time, pos), 1);
 
 	// The normal of the vertex in the world space
 	mat4 modelNormal = transpose(inverse(model));
@@ -41,12 +54,17 @@ void main()
 	vec4 position_projected = projection * view * position;
 
 	// Fill the parameters sent to the fragment shader
+	// Random color - not using vertex_color
+	vec3 light = vec3(1, 0.88, 0.36);
+	vec3 dark = vec3(0.7, 0.48, 0.18);
+	float lambda = pow(rand(floor(10 * time) + pos[0] + pos[1]), 4);
+
 	fragment.position = position.xyz;
 	fragment.normal   = normal.xyz;
-	fragment.color = vertex_color;
-	fragment.alpha = mod(time, 1);
-	// TODO
-	fragment.uv = vertex_uv + time / 40; // we move the texture
+	fragment.color = (1 - lambda) * light + lambda * dark;
+	float eps_alpha = (rand(floor(10 * time) * gl_InstanceID) - 0.5) / 10;
+	fragment.alpha = alpha[0] + eps_alpha;
+	fragment.uv = vertex_uv;
 
 	// gl_Position is a built-in variable which is the expected output of the vertex shader
 	gl_Position = position_projected; // gl_Position is the projected vertex position (in normalized device coordinates)
