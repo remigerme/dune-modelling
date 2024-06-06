6182366d5efab05c8ab1d88431ee6500b5d6808b
remi.germe <remi.germe@gmail.com>
2024-06-06 23:47:25 +0200

    progress towards final scene

--- src/scene.cpp
+++ src/scene.cpp
@@ -2,58 +2,45 @@
 
 using namespace cgp;
 
-float max(float a, float b) {
-    if (a < b)
-        return b;
-    else
-        return a;
-}
-
-// This function is called only once at the beginning of the program
-// This function can contain any complex operation that can be pre-computed once
 void scene_structure::initialize() {
-    // Set the behavior of the camera and its initial position
-    // ********************************************** //
+    // Camera control
     camera_control.initialize(inputs, window);
-    camera_control.set_rotation_axis_z(); // camera rotates around z-axis
-    //   look_at(camera_position, targeted_point, up_direction)
     camera_control.look_at(
-        {3.0f, -2.0f, 1.5f} /* position of the camera in the 3D scene */,
-        {0, 0, 0} /* targeted point in 3D scene */,
+        {10, 10, 30} /* position of the camera in the 3D scene */,
+        {0, 0, 30} /* targeted point in 3D scene */,
         {0, 0, 1} /* direction of the "up" vector */);
+    // Default parameters value
+    camera_control.translation_speed = 10;
+    camera_projection.field_of_view = Pi / 2.0f;
 
     // General information
     display_info();
-
-    // Create the global (x,y,z) frame
-    // global_frame.initialize_data_on_gpu(mesh_primitive_frame());
-
-    // Create the shapes seen in the 3D scene
-    // ********************************************** //
 
     // Terrain : the ground (dunes)
     vec3 ground_scale = 200 * vec3{1, 1, 1}; // scaling factor
     float ground_uv_range = 3; // used for repetition of the texture
     ground = Ground(ground_scale, ground_uv_range);
+    // Load a shader from a file
+    ground.ground_drawable.shader.load(
+        project::path + "shaders/ground/thumper.vert.glsl",
+        project::path + "shaders/mesh/mesh.frag.glsl");
 
     // Terrain : the surrounding cliff
     vec3 cliff_scale = vec3{200, 200, 250}; // scaling factor
-    float cliff_uv_range = 30; // used for repetition of the texture
+    float cliff_uv_range = 4; // used for repetition of the texture
     cliff = Cliff(cliff_scale, cliff_uv_range);
     // To be sure to be below the dunes
     cliff.cliff_drawable.model.set_translation({0, 0, -100});
 
-    // Default FOV
-    camera_projection.field_of_view = Pi / 2.0f;
-
-    // Load a shader from a file
-    opengl_shader_structure shader_marteleur;
-    shader_marteleur.load(
-        project::path + "shaders/shading_marteleur/shading_marteleur.vert.glsl",
-        project::path + "shaders/mesh/mesh.frag.glsl");
-    // ground.shader = shader_marteleur;
-
     marteleur = Marteleur(1);
+    float xy[] = {0, 0};
+    marteleur.set_position(xy, ground);
+
+    float worm_scale = 7.0f;
+    worm = Worm(worm_scale);
+
+    int nb_dust_particles = 2000;
+    worm_dust = WormDust(nb_dust_particles, worm);
 }
 
 /*
@@ -68,39 +55,48 @@ void scene_structure::display_frame() {
 
     // Updating variables
     timer.update();
-    float X = 0;
-    float Y = 0;
 
+    // Updating thumper
     marteleur.marteleur["haut"].transform_local.translation = {
-        0, 0, 0.85 + max(0.15, 0.4 * cos(timer.t) * cos(timer.t))};
-    // float hauteur = evaluate_terrain_height(0, 0);
-    marteleur.marteleur["tube_central"].transform_local.translation = {
-        X, Y, 0}; // en vrai, mettre terrain_height
+        0, 0, 0.85 + std::max(0.15, 0.4 * cos(timer.t) * cos(timer.t))};
+    marteleur.set_position(marteleur_xy, ground);
+    marteleur.marteleur.update_local_to_global_coordinates();
+
+    // Updating worm
+    float xt = timer.t;
+    float yt = 3 * timer.t;
+    float zt = ground.get_height(xt, yt);
+    vec3 pw = {xt, yt, ground.get_height(xt, yt)};
+    worm.set_position(pw.x / worm.scale, pw.y / worm.scale, ground);
+    worm.worm.update_local_to_global_coordinates();
 
     // Passing uniform variables to shaders
-    marteleur.marteleur.update_local_to_global_coordinates();
     environment.uniform_generic.uniform_float["time"] = timer.t;
-    environment.uniform_generic.uniform_float["X"] = X;
-    environment.uniform_generic.uniform_float["Y"] = Y;
+    environment.uniform_generic.uniform_float["xm"] = marteleur_xy[0];
+    environment.uniform_generic.uniform_float["ym"] = marteleur_xy[1];
+    environment.uniform_generic.uniform_float["statusm"] = marteleur_status;
+    environment.uniform_generic.uniform_vec3["pos_worm"] = pw;
 
     // Drawing objects
-    draw(ground.ground_drawable, environment);
+    ground.display(environment);
     draw(cliff.cliff_drawable, environment);
-    draw(worm, environment);
-    draw(marteleur.marteleur, environment);
-
-    // conditional display of the global frame (set via the GUI)
-    if (gui.display_frame)
-        draw(global_frame, environment);
+    draw(worm.worm, environment);
+    worm_dust.display(environment);
+    if (marteleur_status)
+        draw(marteleur.marteleur, environment);
 }
 
 void scene_structure::display_gui() {
-    ImGui::Checkbox("Frame", &gui.display_frame);
-    ImGui::Checkbox("Wireframe", &gui.display_wireframe);
-    ImGui::SliderFloat("Ambient", &cliff.cliff_drawable.material.phong.ambient,
-                       0, 5);
-    ImGui::SliderFloat("Diffuse", &cliff.cliff_drawable.material.phong.diffuse,
-                       0, 5);
+    ImGui::Checkbox("Enable thumper", &marteleur_status);
+    // We need ground_scale.x == ground_scale.y
+    float s = ground.ground_scale.x;
+    ImGui::SliderFloat2("Thumper position (x, y)", marteleur_xy, -s, s);
+    ImGui::SliderFloat("FOV", &camera_projection.field_of_view,
+                       10.0f * Pi / 180.0f, 150.0f * Pi / 180.0f);
+    ImGui::SliderFloat("Translation speed", &camera_control.translation_speed,
+                       1, 100);
+    ImGui::SliderFloat("Rotation speed", &camera_control.rotation_speed, 0.2f,
+                       10);
 }
 
 void scene_structure::mouse_move_event() {
@@ -119,14 +115,12 @@ void scene_structure::idle_frame() {
 
 void scene_structure::display_info() {
     std::cout << "\nCAMERA CONTROL:" << std::endl;
-    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << "------------------------------------------" << std::endl;
     std::cout << camera_control.doc_usage() << std::endl;
-    std::cout << "-----------------------------------------------\n"
-              << std::endl;
+    std::cout << "------------------------------------------\n" << std::endl;
 
     std::cout << "\nSCENE INFO:" << std::endl;
-    std::cout << "-----------------------------------------------" << std::endl;
-    std::cout << "Introductory scene for INF443." << std::endl;
-    std::cout << "-----------------------------------------------\n"
-              << std::endl;
+    std::cout << "------------------------------------------" << std::endl;
+    std::cout << "Dune modelling project for INF443." << std::endl;
+    std::cout << "------------------------------------------\n" << std::endl;
 }
